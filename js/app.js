990e3b0687d19f5f8dcc8fea5ed6b4bc28985757
ArtBlack <0960469634kvstrt@gmail.com>
2023-06-04 16:41:16 +0200

    last

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    const ul = document.querySelector('.ul__question')
+    ul.addEventListener('click', (e) => {
+        e = e.target
+        if (e.tagName === 'LI') {
+            e.classList.toggle('li__question--act')
+        }
+    })
 })
