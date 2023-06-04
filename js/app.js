34ddbe856ab01dc4be3cf27d36b4812cb96f50ff
ArtBlack <0960469634kvstrt@gmail.com>
2023-06-04 19:24:35 +0200

    header

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const ul = document.querySelector('.ul__question')
+    const header = document.querySelector('.header')
+    const ham = document.querySelector('.ham__header')
+    const nav = document.querySelector('.nav__header')
     ul.addEventListener('click', (e) => {
         e = e.target
         if (e.tagName === 'LI') {
             e.classList.toggle('li__question--act')
         }
+    })
+    ham.addEventListener('click', (e) => {
+        ham.classList.toggle('ham__header--act')
+        nav.classList.toggle('nav__header--act')
+        header.classList.toggle('header--act')
     })
 })
